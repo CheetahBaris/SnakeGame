d41c1b4e0c6656fcfc96df83737b8c7937b6531f
nekit155rybin <nekit155rybin@gmail.com>
2021-11-21 18:23:09 +0300

    Add project files.

---
+++ Intermediate/Build/Win64/SnakeGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.30.30705
+#include "D:/Snake Game UE4/SnakeGame/Intermediate/Build/Win64/SnakeGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Source/SnakeGame/SnakeBase.cpp
@@ -0,0 +1,82 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SnakeBase.h"
+#include "SnakeElementBase.h"
+
+// Sets default values
+ASnakeBase::ASnakeBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	ElementSize = 100.f;
+	MovmentSpeed = 10.f;
+	LastMoveDirection = EMovmentDirection::DOWM;
+}
+
+// Called when the game starts or when spawned
+void ASnakeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	SetActorTickInterval(MovmentSpeed);
+	AddSnakeElement(5);
+	
+}
+
+// Called every frame
+void ASnakeBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	Move( );
+
+}
+
+void ASnakeBase::AddSnakeElement(int ElementsNum)
+{	
+	for (int i = 0; i < ElementsNum; ++i)
+	{
+		FVector NewLocation(SnakeElements.Num() * ElementSize, 0, 0);
+		FTransform NewTransform(NewLocation);
+		ASnakeElementBase* NewSnakeElem = GetWorld()->SpawnActor<ASnakeElementBase>(SnakeElementClass, NewTransform  );		 
+		SnakeElements.Add(NewSnakeElem);
+
+	}
+}
+
+void ASnakeBase::Move( )
+{
+	FVector MovmentVector(ForceInitToZero);
+	float MovmentSpeed = ElementSize;
+		 
+	switch (LastMoveDirection)
+	{
+
+	case EMovmentDirection::UP:
+		MovmentVector.X += MovmentSpeed;
+		break;
+	case EMovmentDirection::DOWM:
+		MovmentVector.X -= MovmentSpeed;
+		break;
+	case EMovmentDirection::LEFT:
+		MovmentVector.Y += MovmentSpeed;
+		break;
+	case EMovmentDirection::RIGHT:
+		MovmentVector.Y -= MovmentSpeed;
+		break;
+	}
+
+
+	 
+
+	for (int i = SnakeElements.Num() - 1; i > 0; i--)
+	{
+		auto CurrentElement = SnakeElements[i];
+		auto PrevElement = SnakeElements[i - 1];
+		FVector PrevLocation = PrevElement->GetActorLocation();
+		CurrentElement->SetActorLocation(PrevLocation);
+
+	}
+
+	SnakeElements[0]->AddActorWorldOffset(MovmentVector); //AddActorWorldOffset(MovmentVector);
+}
+
